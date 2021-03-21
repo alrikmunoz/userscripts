96f6048f3d22091249147fb01827565933fe8b29
alrikmunoz <50486916+alrikmunoz@users.noreply.github.com>
2021-03-21 19:18:29 +0100

    Consolidated embedded YT autoplay scripts into one

--- youtubeEmbeddedEurovision/youtubeEmbeddedToYT.user.js
+++ youtubeEmbeddedEurovision/youtubeEmbeddedToYT.user.js
@@ -1,12 +1,20 @@
 // ==UserScript==
 // @name         Eurovision.tv directly to youtube video
 // @version      0.2
-// @description  Automatically press the "Youtube"-button in embedded youtube videos to go and watch video on youtube website instead
+// @description  Partial credit to jcunews. Automatically play and press the "Youtube"-button in embedded youtube videos to go and watch video on youtube website instead
 // @author       alrikmunoz
+// @match        https://www.youtube.com/embed?*
 // @match        https://www.youtube.com/embed/*
+// @match        https://www.youtube-nocookie.com/embed?*
+// @match        https://www.youtube-nocookie.com/embed/*
 // @require      file:///Users/alrik/Desktop/Hemma/Programmering/userscripts/youtubeEmbeddedEurovision/youtubeEmbeddedToYT.user.js
 // ==/UserScript==
 
-document.querySelector(".ytp-large-play-button").click();
-//setTimeout(function(){document.querySelector("button.ytp-play-button.ytp-button").click();}, 1000); //plays embedded video, not sure why originally added, so removed it now
-document.querySelector("a.ytp-youtube-button.ytp-button.yt-uix-sessionlink").click();
+// if embedded youtube url does NOT contain query parameter "autoplay=1"
+if (!(/[?&]autoplay=1/).test(location.search)) {
+	var playButton = document.querySelector(".ytp-large-play-button");
+	var youtubeButton = document.querySelector("a.ytp-youtube-button.ytp-button.yt-uix-sessionlink")
+
+	playButton.click();  // click on play button
+	youtubeButton.click();  // click on "Youtube"-button in video player
+}

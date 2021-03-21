596d58f464eee514b889d30f358de79c9b21592f
alrikmunoz <50486916+alrikmunoz@users.noreply.github.com>
2021-03-21 19:49:46 +0100

    Script now waits to run until tab is in focus

--- youtubeEmbeddedEurovision/youtubeEmbeddedToYT.user.js
+++ youtubeEmbeddedEurovision/youtubeEmbeddedToYT.user.js
@@ -10,11 +10,27 @@
 // @require      file:///Users/alrik/Desktop/Hemma/Programmering/userscripts/youtubeEmbeddedEurovision/youtubeEmbeddedToYT.user.js
 // ==/UserScript==
 
-// if embedded youtube url does NOT contain query parameter "autoplay=1"
-if (!(/[?&]autoplay=1/).test(location.search)) {
-	var playButton = document.querySelector(".ytp-large-play-button");
-	var youtubeButton = document.querySelector("a.ytp-youtube-button.ytp-button.yt-uix-sessionlink")
 
-	playButton.click();  // click on play button
-	youtubeButton.click();  // click on "Youtube"-button in video player
+// triggers only once document is visible
+var timer = setInterval(() => {
+	if (!document.hidden) {
+		console.log("Opening video!")
+		play();
+		clearInterval(timer);
+	} else {
+		console.log("Tab not in focus!");
+	}
+}, 100);
+
+
+// opens embedded youtube videos in another tab
+function play() {
+	// if embedded youtube url does NOT contain query parameter "autoplay=1"
+	if (!(/[?&]autoplay=1/).test(location.search)) {
+		var playButton = document.querySelector(".ytp-large-play-button");
+		var youtubeButton = document.querySelector("a.ytp-youtube-button.ytp-button.yt-uix-sessionlink")
+
+		playButton.click();  // click on play button
+		youtubeButton.click();  // click on "Youtube"-button in video player
+	}
 }
